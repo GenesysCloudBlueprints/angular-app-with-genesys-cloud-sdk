cf6d0cbbed562e14d4f6fd69582e4be8c694e8c5
hero2bash <zero.bash@gmail.com>
2021-07-12 11:09:10 +0800

    Add new build

--- genesys-cloud-sample/src/environments/environment.prod.ts
+++ genesys-cloud-sample/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   GENESYS_CLOUD_CLIENT_ID: 'e7de8a75-62bb-43eb-9063-38509f8c21af',
-  REDIRECT_URI: 'https://princemerluza.github.io/platform-sdk-in-angular-blueprint/'
+  REDIRECT_URI: 'https://genesyscloudblueprints.github.io/angular-app-with-genesys-cloud-sdk/'
 };
